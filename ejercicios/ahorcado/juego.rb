f3b70744da2d25947a62b63890e90d0598f93dfd
Pablo <loiaconopablo@gmail.com>
2013-09-27 17:57:49 +0200

    modificaciones

--- ejercicios/ahorcado/juego.rb
+++ ejercicios/ahorcado/juego.rb
@@ -3,10 +3,12 @@ class Juego
 	def set_palabra(a_word)
 		@word = a_word
 		@errores = 0
+		@aciertos = Array.new(self.obtener_longitud, "__")
 	end
 
 	def verificar_letra	(a_letter)
 		if @word.include? a_letter
+			@aciertos
 			return true
 		else
 			@errores +=1
@@ -21,5 +23,20 @@ class Juego
 	def get_errores
 		@errores
 	end
+
+	def get_aciertos
+		@aciertos
+
+
+
+	def status
+
+
+	def fin_juego
+		if @errores = = 10
+			retun 'Perdiste, la palabra era '+@word
+		end
+	end
+
 
 end
--- ejercicios/ahorcado/juego_spec.rb
+++ ejercicios/ahorcado/juego_spec.rb
@@ -58,13 +58,51 @@ describe 'Juego' do
 			@juego.get_errores().should eq 1
 		end	
 
-
 		it 'deberia devolver 2 cuando pregunto mal 2 veces y la palabra es oso' do
 			@juego.set_palabra 'oso'
 			@juego.verificar_letra 'a'
 			@juego.verificar_letra 'j'
 			@juego.get_errores().should eq 2
-		end	
+		end
+	end
+
+	describe 'aciertos' do
+
+			it 'deberia devoler un array con los __ vacios al inicio del juego' do
+				@juego.set_palabra 'prueba'
+				@juego.get_aciertos().should eq []
+			end
+
+			it 'deberia devolver un array con la letra que acertamos' do
+				@juego.set_palabra 'agua'
+				@juego.verificar_letra 'g'
+				@juego.get_aciertos().should eq [g]
+			end
+
+			it 'deberia devolver un array con las letras que acertamos' do
+				@juego.set_palabra 'agua'
+				@juego.verificar_letra 'g'
+				@juego.verificar_letra 'a'
+				@juego.get_aciertos().should eq ["g", 'a', 'a']
+			end
+
+
+	describe 'estado del Juego' do
+
+		it 'deberia devolver la cantidad de vidas que nos quedan si todavia no perdimos ni ganamos' do
+			@juego.set_palabra 'oso'
+			@juego.status().should eq 'Te quedan 10 intentos.'
+			@juego.verificar_letra 'a' 	# Letra erronea
+			@juego.status().should eq 'Te quedan 9 intentos.'
+			@juego.verificar_letra 's'	# Letra Correcta
+			@juego.status().should eq 'Te quedan 9 intentos.'
+			@juego.verificar_letra 'c'	# Letra erronea
+			@juego.verificar_letra 'd'	# Letra erronea
+			@juego.status().should eq 'Te quedan 7 intentos.'
+
+		end
+
+		it 'deberia devolver que ganamos el juego si acertamos'
 
 	end		
 
